a5c5d89d8521a1f0917e23ea3c796d436c168401
Patrick Sullivan <patrick.sullivan@hey.com>
2022-04-30 15:16:56 -0500

    start unsat

--- src/Ground.hs
+++ src/Ground.hs
@@ -1,5 +1,6 @@
 module Ground
   ( groundTerms,
+    groundTermPermutations,
   )
 where
 
@@ -14,23 +15,33 @@ groundTerms objs fns n =
     then TObj <$> objs
     else do
       (FnConst name arity) <- fns
-      let argCombinations = groundArgCombinations objs fns arity (n - 1)
+      let argCombinations = groundTermPermutations objs fns arity (n - 1)
       TFn (FnConst name arity) <$> argCombinations
 
--- | Generate all combinations of arguments for a function with the given arity
--- where each argument has a maximum "depth" of `n`.
-groundArgCombinations :: [ObjConst] -> [FnConst] -> Int -> Int -> [[Term]]
+-- | Generate all `m`-length permunations of ground terms, where each term has a
+-- "depth" of `n`.
+groundTermPermutations :: [ObjConst] -> [FnConst] -> Int -> Int -> [[Term]]
 -- A nullary function can have exactly one combination of depth-0 arguments, the
 -- empty list of arguments.
-groundArgCombinations objs fns 0 0 = [[]]
+groundTermPermutations objs fns 0 0 = [[]]
 -- A nullary function has no combinations of depth-n arguments when n > 0.
-groundArgCombinations objs fns 0 n = []
-groundArgCombinations objs fns arity n = do
+groundTermPermutations objs fns 0 n = []
+groundTermPermutations objs fns arity n = do
   -- for each depth from 0 to n
   depth <- [0 .. n]
   -- for each term with the given depth
   head <- groundTerms objs fns depth
   -- for each combination of (arity - 1) args where each arg has a max depth of
   -- (n - depth)
-  tail <- groundArgCombinations objs fns (arity - 1) (n - depth)
+  tail <- groundTermPermutations objs fns (arity - 1) (n - depth)
   return $ head : tail
+
+exp1 =
+  [ [TFn (FnConst {fnConstName = "f", fnConstArity = 1}) [TObj (ObjConst {objConstName = "c"})]],
+    [TFn (FnConst {fnConstName = "f", fnConstArity = 1}) [TObj (ObjConst {objConstName = "d"})]]
+  ]
+
+exp2 =
+  [ [TFn (FnConst {fnConstName = "f", fnConstArity = 1}) [TFn (FnConst {fnConstName = "f", fnConstArity = 1}) [TObj (ObjConst {objConstName = "c"})]]],
+    [TFn (FnConst {fnConstName = "f", fnConstArity = 1}) [TFn (FnConst {fnConstName = "f", fnConstArity = 1}) [TObj (ObjConst {objConstName = "d"})]]]
+  ]
---
+++ src/Unsat.hs
@@ -0,0 +1,20 @@
+module Unsat where
+
+import Ast (Term, Var)
+import Data.Map as Map
+import Ground (groundTermPermutations)
+
+-- type Substitution = Map Var Term
+
+herbLoop mfn tfn fo0 objs fns freeVars n fo tried perms = do
+  putStrLn $ show (length tried) ++ " ground instances tried; " ++ show (length fo) ++ " items in list"
+  case perms of
+    [] ->
+      let perms' = groundTermPermutations objs fns (length freeVars) n
+       in herbLoop mfn tfn fo0 objs fns freeVars (n + 1) fo tried perms'
+    p : ps ->
+      let fo' = mfn fo0 undefined fo
+       in do
+            if not (tfn fo')
+              then return (p : tried)
+              else herbLoop mfn tfn fo0 objs fns freeVars n fo' (p : tried) perms
